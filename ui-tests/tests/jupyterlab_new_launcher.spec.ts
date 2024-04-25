77c6f596b008882d7f1776941528b7ab93672ea4
krassowski <5832902+krassowski@users.noreply.github.com>
2024-04-25 18:35:00 +0100

    Add missing dot, restore wider button

--- ui-tests/tests/jupyterlab_new_launcher.spec.ts
+++ ui-tests/tests/jupyterlab_new_launcher.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@jupyterlab/galata';
 
 test('should render new launcher', async ({ page }) => {
-  const launcher = page.locator('jp-LauncherBody');
+  const launcher = page.locator('.jp-LauncherBody');
   expect(await launcher.screenshot()).toMatchSnapshot('launcher.png');
 });
